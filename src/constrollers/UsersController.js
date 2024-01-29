d0eb74aeae793237043c95d77aec5b6885c03f8b
arthur <arthuralbduarte@yahoo.com>
2024-01-29 12:05:43 -0300

    Adicionando o App Error e configurando

--- src/constrollers/UsersController.js
+++ src/constrollers/UsersController.js
@@ -1,3 +1,5 @@
+const AppError = require("../utils/AppError");
+
 class UsersController {
  /**
   * index - GET para listar vários registros
@@ -13,7 +15,12 @@ class UsersController {
     
     const {name, email, password} = request.body;
 
-    response.json({name, email, password});}
+    if(!name) {
+        throw new AppError ("O nome é obrigatório!");
+    }
+
+    response.status(201).json({name, email, password});
+    }
 }
 
 module.exports = UsersController;
