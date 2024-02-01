c69b5cb7ca5d284098e1d8ea7cc8aad94caa5a6a
arthur <arthuralbduarte@yahoo.com>
2024-02-01 11:46:11 -0300

    Adicionar o banco de dados SQL, criando a tabela de usuarios e automatizar a crianção da mesma.

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 require('express-async-errors');
+
+const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
 const express = require("express");
 
@@ -10,6 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+migrationsRun(); 
 
 app.use((error, request, response, next) => {
     if (error instanceof AppError) {
