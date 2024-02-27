39b2f5355a3e3c6fa4695cf44db3ba853bfbdccd
arthur <arthuralbduarte@yahoo.com>
2024-02-26 21:24:41 -0300

    Adicionar parametros

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -5,6 +5,7 @@ const sqliteConnection = require('../database/sqlite')
 class UsersController {
   async create(request, response) {
     const { name, email, password } = request.body
+
     const database = await sqliteConnection()
     const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
 
